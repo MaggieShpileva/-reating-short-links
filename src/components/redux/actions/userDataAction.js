6faa1af0037b0389dd64c8e38e931d8fc07347e2
MaggieShpileva <shpileva.rita@gmail.com>
2022-09-07 23:20:39 +0300

    Update finaly project

--- src/components/redux/actions/userDataAction.js
+++
@@ -1,6 +0,0 @@
-export const setUserData = (username) => {
-  return {
-    type: 'SET_USERNAME',
-    payload: username,
-  };
-};
--- src/components/redux/store/ConfigeStore.js
+++
@@ -1,3 +0,0 @@
-import { createStore } from 'redux';
-import { userData } from '../reducers/userData';
-export const store = createStore(userData);
